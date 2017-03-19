5c6b12caffbf775444e3d838baff692eb4e412b7
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-03-19 11:44:23 -0700

    checkpoint-5-controllers

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,47 @@
+// Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+ };
+ 
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // Assignment Album
+ var darkSide = {
+     title: 'Dark Side of the Moon',
+     artist: 'Pink Floyd',
+     label: 'EMI',
+     year: '1973',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Breath', duration: '2:43' },
+         { title: 'Time', duration: '6:53' },
+         { title: 'The Great Gig in the Sky', duration: '4:15'},
+         { title: 'Money', duration: '6:30' },
+         { title: 'Us and Them', duration: '7:51'}
+     ]
+ };
