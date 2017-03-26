3f8be02f5da1208ba6743a1e3850642e247fe230
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-03-26 15:21:19 -0700

    checkpoint-9-directives

---
+++ app/scripts/directives/seekBar.js
@@ -0,0 +1,77 @@
+(function() {
+  function seekBar($document) {
+    /*
+    * continuously keep track of % offset
+    */
+    var calculatePercent = function(seekBar, event) {
+      var offsetX = event.pageX - seekBar.offset().left;
+      var seekBarWidth = seekBar.width();
+      var offsetXPercent = offsetX / seekBarWidth;
+      offsetXPercent = Math.max(0, offsetXPercent);
+      offsetXPercent = Math.min(1, offsetXPercent);
+      return offsetXPercent;
+    };
+
+
+    return {
+      templateUrl: '/templates/directives/seek_bar.html',
+      replace: true,
+      restrict: 'E',
+      scope: { },
+      link: function(scope, element, attributes) {
+        scope.value = 0;
+        scope.max = 100;
+
+        /*
+        * private method
+        * @desc = assign seekBar to $(element)
+        */
+        var seekBar = $(element);
+
+        /*
+        *private function
+        */
+        var percentString = function(){
+          var value = scope.value;
+          var max = scope.max;
+          var percent = value / max * 100;
+          return percent + "%";
+        };
+
+        /*
+        *public function
+        *returns width CSS style
+        */
+        scope.fillStyle = function() {
+          return {width: percentString()};
+        };
+
+        /*
+        * updates scope.value
+        */
+        scope.onClickSeekBar = function(event) {
+          var percent = calculatePercent(seekBar, event);
+          scope.value = percent * scope.max;
+        };
+
+        scope.trackThumb = function() {
+           $document.bind('mousemove.thumb', function(event) {
+               var percent = calculatePercent(seekBar, event);
+               scope.$apply(function() {
+                   scope.value = percent * scope.max;
+               });
+           });
+
+           $document.bind('mouseup.thumb', function() {
+               $document.unbind('mousemove.thumb');
+               $document.unbind('mouseup.thumb');
+           });
+       };
+      }
+    };
+  }
+
+  angular
+    .module('blocJams')
+    .directive('seekBar', ['$document', seekBar]);
+})();
